7979fc1f38246be3a277eb07c7ea0f4938ebcc28
Yeong <ycbaek@gmail.com>
2022-01-07 10:07:55 -0800

    first commit

---
+++ project1.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import os
+
+
+
+
+def load_columns (path_Models):
+    
+    list_Models = os.listdir(path_Models)
+    col_applied_file = [file for file in list_Models if 'col_applied'in file][0]
+    col_applied_filename = path_Models+'{}'.format(col_applied_file)
+    df_col_applied = pd.read_csv(col_applied_filename)
+    col_applied = list(df_col_applied["col_name"])
+    col_applied = [ele.lower() for ele in col_applied]
+    
+    return col_applied
+
